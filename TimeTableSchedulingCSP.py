816eddf4b0c14f61860107148ea55ee873bbd77f
kb <kritikabansal21.11@gmail.com>
2018-10-15 20:43:50 +0530

    Added randomisation in TimeTableSchedulingCSP.py

--- TimeTableSchedulingCSP.py
+++ TimeTableSchedulingCSP.py
@@ -6,6 +6,7 @@ Constraints:
 """
 
 import numpy as np
+import random
 
 R = 5
 C = 8
@@ -67,6 +68,100 @@ def ConstraintsSatisfationProblem(course_prof_dict):
         for j in range(0, len(time_table[0])):
             print(time_table[i][j], end=' ')
     print()
+
+    # verifying if constraints are violated
+    print('This should be 1 for non-violating TT: ', evaluate_fitness_function(time_table, course_prof_dict))
+    print()
+
+
+def ModifiedConstraintsSatisfationProblem(course_prof_dict):
+    global time_table
+    available_slots = []
+
+    for i in range(0, R):
+        for j in range(0,C):
+            for k in range(0, N):
+                available_slots.append((i,j,k))
+
+    no_of_lectures = 2*M
+    course_day = {}
+    prof_day_slot = {}
+
+    for i in range(1, M+1):
+        course_day[i] = -1
+
+    for i in range(1, P+1):
+        prof_day_slot[i] = []
+
+    lecture = 1
+    while no_of_lectures>0:
+        random.shuffle(available_slots)
+        i,j,k = available_slots[0]
+        if course_day[lecture] == -1 and (i,j) not in prof_day_slot[course_prof_dict[lecture]]:
+            time_table[i][j][k] = lecture
+            course_day[lecture] = i
+            prof_day_slot[course_prof_dict[lecture]].append((i,j))
+            available_slots.remove((i,j,k))
+        elif course_day[lecture] != -1 and (i,j) not in prof_day_slot[course_prof_dict[lecture]]:
+            day = course_day[lecture]
+            if i!=day:
+                time_table[i][j][k] = lecture
+                course_day[lecture] = i
+                prof_day_slot[course_prof_dict[lecture]].append((i, j))
+                available_slots.remove((i,j,k))
+            else:
+                continue
+        else:
+            continue
+
+        if no_of_lectures % 2 != 0:
+            lecture += 1
+        no_of_lectures -= 1
+
+    for i in range(0, len(time_table)):
+        print('\nDAY :', i + 1)
+        for j in range(0, len(time_table[0])):
+            print(time_table[i][j], end=' ')
+    print()
+
+    #verifying if constraints are violated
+    print('This should be 1 for non-violating TT: ', evaluate_fitness_function(time_table, course_prof_dict))
+
+
+def evaluate_fitness_function(chromosome, course_prof_dict):
+    c = 0
+    '''
+       *. When lecture for a subject is scheduled more than once in a day, then penalise with the count with which it exceeds
+       b. When lecture for a subject is scheduled more than twice in a week, then penalise with the count with which it exceeds
+       *. When a lecture taught by same professor is scheduled in same time slot.
+    '''
+    lectures_in_a_week = []
+    for i in range(0, R):
+        lectures_in_a_day = []
+        for j in range(0, C):
+            prof_ids = []
+            lectures_in_a_slot = []
+            for k in range(0, N):
+                if chromosome[i][j][k] != 0:
+                    course_id =  chromosome[i][j][k]
+                    lectures_in_a_slot.append(course_id)            #keeps courseids whose lectures are in a particular slot
+                    lectures_in_a_day.append(course_id)             #keeps courseids whose lectures are in a particular day
+                    prof_ids.append(course_prof_dict[course_id])    #keeps profids whose lectures are in a particular slot
+                    lectures_in_a_week.append(course_id)            #keeps courseids whose lectures are in a particular week
+
+            if len(lectures_in_a_slot) > len(set(lectures_in_a_slot)):
+                c += (len(lectures_in_a_slot) - len(set(lectures_in_a_slot)))   #avoids same course lectures in same slot
+            if len(prof_ids) > len(set(prof_ids)):
+                c += (len(prof_ids) - len(set(prof_ids)))                       #avoids same prof teaching in same slot
+        if len(lectures_in_a_day) > len(set(lectures_in_a_day)):
+            c += (len(lectures_in_a_day)-len(set(lectures_in_a_day)))           #avoids same course lectures in same day
+
+    cid, count = np.unique(lectures_in_a_week, return_counts=True)
+    for val in count:
+        c += abs(val-2)
+
+    return float(1/(1+c))
+
 
 c_p_dict = {1:1, 2:1, 3:2, 4:2, 5:3, 6:3, 7:4, 8:4, 9:5, 10:5, 11:6, 12:6, 13:7, 14:7, 15:8, 16:8, 17:9, 18:10, 19:11, 20:12}
 #, 21:16, 22:17, 23:18, 24:19, 25:20
